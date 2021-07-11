b38bbdaec1d3aacc5a21ea686fdc28e14705a486
0xsweat <83222877+0xsweat@users.noreply.github.com>
2021-07-11 04:55:56 -0700

    Add files via upload

---
+++ cmo.py
@@ -0,0 +1,21 @@
+print("cmo text version 1.0")
+print("coded by 0x.sweat")
+print("program.cmo.quit closes the text editor \nit also saves the file unless you don't want to, if you don't then leave the save blank")
+contents = []
+code = ''
+
+new_lines = ''
+linespace = "\n"
+def save ():
+    fname = input("file to write : ")
+    with open(fname, "w+") as f:
+        f.write((code))
+        f.close()
+
+while new_lines != 'program.cmo.quit':
+        new_lines = input("# ")
+        if new_lines != 'program.cmo.quit':
+                contents.append(new_lines)
+                contents.append(linespace)
+code = ''.join(contents)
+save()
---
+++ readme.txt
@@ -0,0 +1,5 @@
+This is 0x.sweat's text editor
+
+type in
+program.cmo.quit
+if you want to close the program
