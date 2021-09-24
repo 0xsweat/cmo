ff890f5dd79f50edbcd98715bad2e4b27f87ab6d
0xsweat <83222877+0xsweat@users.noreply.github.com>
2021-09-23 21:40:59 -0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo -e "${Red}must run as root" 
+   exit 1
+fi
+pip install colorama
+mkdir /cmo
+cp cmo.py /cmo
+echo "alias cmo='python /cmo/cmo.py'" >> ~/.bashrc
+echo "alias cmo='python /cmo/cmo.py'" >> ~/.zshrc
+Cyan='\033[1;36m'
+echo -e "${Cyan}Cmo can now be started with just typing in 'cmo'"
